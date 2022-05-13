f8159c913e1cfb0d5d2e9757ca899c20ff11e1ea
j5x <32881@ma-web.nl>
2022-05-13 11:47:15 +0200

    first commit

---
+++ EnumOpdracht/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    internal class Program
+    {
+        enum Colors
+        {
+            Red = 9,
+            Green = 8,
+            Blue = 10,
+            Orange = 7
+        }
+
+
+    }
+}
