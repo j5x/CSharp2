807938128bca079e9fa50732c77c00978d303301
j5x <32881@ma-web.nl>
2022-06-10 11:44:18 +0200

    updated

---
+++ constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
