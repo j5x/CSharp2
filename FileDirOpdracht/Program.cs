2f59f236a0da4296e3bc09132dc2dfeaecb8c820
mo <mosawer.asg@outlook.com>
2022-06-20 14:49:29 +0200

    Files&Directories

---
+++ FileDirOpdracht/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = File.ReadAllText(@"C:\Users\Mosaw\Desktop\test\CSharp2\FileDirOpdracht\Text.txt");
+            string[] lines = File.ReadAllLines(@"C:\Users\Mosaw\Desktop\test\CSharp2\FileDirOpdracht\Text.txt");
+            //Console.WriteLine(text);
+        }
+    }
+}
---
+++ FileIOOpdracht/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo myDir = new DirectoryInfo(@"C:\Users\Mosaw\Desktop\test\CSharp2");
+            foreach (var sub in myDir.GetFiles())
+            {
+                Console.WriteLine(sub.Name);
+            }
+        }
+    }
+}
